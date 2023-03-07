4418194cbdfb041d9fdaaf18eea4030a00a477ba
Pranav-mate <126973653+Pranav-mate@users.noreply.github.com>
2023-03-07 06:01:44 +0530

    Update visa.java

--- src/main/java/com/example/bookingms/visa.java
+++ src/main/java/com/example/bookingms/visa.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class visa {
     @GetMapping("/visa")
     public String getData(){
-        return "H1B for us is delayed";
+        return "H1B for UK & USA is delayed";
     }
 }
